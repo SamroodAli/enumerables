8485b88c08947ef6700116d1300805a751e1c662
samroodAli <devsamrood@gmail.com>
2021-04-01 20:02:37 +0530

    negative let array for testing

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -2,6 +2,7 @@ require_relative './../enumerables'
 
 describe 'Enumerable' do
   let(:arr) { [1, 2, 3, 4, 5] }
+  let(:negative) {[1,-2,3,4]}
   describe '#my_each' do
     context 'when no block is given' do
       it 'should return Enumerable' do
